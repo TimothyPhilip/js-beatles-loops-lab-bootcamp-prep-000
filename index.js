3d7db2f2a9dc391f3a3370ca3cf17e0963d84f3f
salubrious-reference-8531 <philippetimothe@gmail.com>
2019-05-07 18:20:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// add solution here
+function theBeatlesPlay(musicians,instruments){
+var array = [musicians[0] + " plays " +instruments[0]]; 
+  for ( var i = 1; i < 4; i++){
+    if (i < 0){
+    array.push(musicians[0] +" plays "+ instruments[0]) 
+    }else{ 
+    array.push(musicians[i] +" plays "+ instruments[i])
+  }
+    }
+    return array
+}
+
+ 
+function johnLennonFacts(facts){
+ let x = 0;
+ var value = [facts[x] + "!!!"];
+ while (x < facts.length) {
+  value.push(facts[x] + "!!!"); x++;
+}
+return value
+}
