f7b5aea2f4a0f64e36f1cba7aafb2f0594d46054
salubrious-reference-8531 <philippetimothe@gmail.com>
2019-05-07 18:44:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ var array = [musicians[0] + " plays " +instruments[0]];
  
 function johnLennonFacts(facts){
  let x = 0;
- var value = [ ];
+ var value = [];
  while (x < facts.length) {
   value.push(facts[x] + "!!!"); x++;
 }
